df5eb6b8b241f3249f97d081290c58368cd63378
Paul Ren <pause.forever@gmail.com>
2022-09-20 16:08:31 +0700

    fix answers by adding hyphen

--- src/DisplayJohn.js
+++ src/DisplayJohn.js
@@ -6,9 +6,9 @@ export function JohnProfile() {
   return (
     <div>
       <ul>
-        <li>Full Stack Developer</li>
-        <li>React.JS Developer</li>
-        <li>Love Mookrata</li>
+        <li>- Full Stack Developer</li>
+        <li>- React.JS Developer</li>
+        <li>- Love Mookrata</li>
       </ul>
     </div>
   );
